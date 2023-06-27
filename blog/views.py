ab6abc05385f885b37e64e220bc030853a4a22f9
Dong Hyuk Park <sts04038@gmail.com>
2023-06-27 14:36:56 +0900

    tag view logic url 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, \
-    DayArchiveView, TodayArchiveView
+    DayArchiveView, TodayArchiveView, TemplateView
 
 from blog.models import Post
 
@@ -44,3 +44,18 @@ class PostTAV(TodayArchiveView):
     model = Post
     date_field = 'modify_dt'
     template_name = 'blog/post_archive_day.html'
+
+class TagCloudTV(TemplateView):
+    template_name = 'taggit/taggit_cloud.html'
+
+class TaggedObjectLV(ListView):
+    template_name = 'taggit/taggit_post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__name=self.kwargs.get('tag'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tagname'] = self.kwargs['tag']
+        return context
