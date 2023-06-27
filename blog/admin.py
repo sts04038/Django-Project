2888832ac382d49816fc77050c2fa0915ff2702a
Dong Hyuk Park <sts04038@gmail.com>
2023-06-27 14:11:48 +0900

    taggit, taggit-templatetags2 추가

--- blog/admin.py
+++ blog/admin.py
@@ -13,3 +13,9 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('modify_dt',)
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return ','.join(tag.name for tag in obj.tags.all())
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "taggit.apps.TaggitAppConfig",
+    "taggit_templatetags2",
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
